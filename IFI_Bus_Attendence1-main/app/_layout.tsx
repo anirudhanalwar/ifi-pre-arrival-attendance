c2be79e200f748ee9ea2de1ac1bdd07da6fad448
anirudhanalwar <sreeanirudhanalwar@gmail.com>
2025-12-15 10:44:02 +0530

    1

--- IFI_Bus_Attendence1-main/app/_layout.tsx
+++ IFI_Bus_Attendence1-main/app/_layout.tsx
@@ -1,12 +1,16 @@
+
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="auto" />
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="splash" /> {/* Splash screen first */}
+        <Stack.Screen name="index" /> {/* Your barcode scanner */}
+        <Stack.Screen name="manual-entry" /> {/* Manual entry screen */}
       </Stack>
     </>
   );
